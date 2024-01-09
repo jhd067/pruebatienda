6d109ed6600e20d1374d5f1f874db73367d9b0ba
jhd067 <jhd067@hotmail.com>
2024-01-08 22:11:11 -0500

    first push mvp

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+// Rutas
+import { APP_ROUTING } from './app.routes'
+
+
+
+
+//Servicios
+
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { RepuestosComponent } from './components/repuestos/repuestos.component';
+import { RepuestoComponent } from './components/repuesto/repuesto.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    RepuestosComponent,
+    RepuestoComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/repuesto/repuesto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repuesto',
+  templateUrl: './repuesto.component.html',
+  styleUrls: ['./repuesto.component.css']
+})
+export class RepuestoComponent {
+
+}
